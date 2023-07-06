8a4be4e10aa9b0144fd093faf4a7d8fd83c8f588
Arezki <mmezrag@techitaway.co>
2023-07-06 14:06:29 +0100

    creating user schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,50 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
-const schema = new mongoose.schema({});
+const schema = new mongoose.schema({
+  name: {
+    type: String,
+    required: [true, "Please enter name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter email"],
+    unique: [true, "Email already taken"],
+    validator: validator.isEmail,
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter password"],
+    minLength: [6, "Password must be at least 6 characters long"],
+    select: false,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  pinCode: {
+    type: Number,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  avatar: {
+    public_id: String,
+    url: String,
+  },
+  otp: Number,
+  otp_expire: Date,
+});
 
 export const User = mongoose.model("User", schema);
