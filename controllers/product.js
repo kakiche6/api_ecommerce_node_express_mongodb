8f23b9399233048639db6bc465b439902dc78b08
Arezki <mmezrag@techitaway.co>
2023-07-09 17:56:23 +0100

    create ^rpduct endpoint

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,8 @@
 import { asyncError } from "../middlewares/error.js";
 import { Product } from "../models/product.js";
 import ErrorHandler from "../utils/error.js";
+import { getDataUri } from "../utils/features.js";
+import cloudinary from "cloudinary";
 
 export const getAllProducts = asyncError(async (req, res) => {
   const products = await Product.find({});
@@ -19,5 +21,32 @@ export const getProductDetails = asyncError(async (req, res) => {
   res.status(200).json({
     success: true,
     product,
+  });
+});
+
+export const createProduct = asyncError(async (req, res) => {
+  const { name, description, category, price, stock } = req.body;
+
+  if (!req.file) return next(new ErrorHandler("Please Add Image", 400));
+
+  const file = getDataUri(req.file);
+  const myCloud = await cloudinary.v2.uploader.upload(file.content);
+  const image = {
+    public_id: myCloud.public_id,
+    url: myCloud.secure_url,
+  };
+
+  await Product.create({
+    name,
+    description,
+    category,
+    price,
+    stock,
+    images: [image],
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "Product Created Successful",
   });
 });
