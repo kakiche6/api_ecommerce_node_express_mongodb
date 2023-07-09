3abb3861bf9cac53450ffd4a038b9a9934bc7a6b
Arezki <mmezrag@techitaway.co>
2023-07-09 16:49:18 +0100

    creating product and category shcema

--- models/product.js
+++ models/product.js
@@ -1,5 +1,36 @@
 import mongoose from "mongoose";
 
-const schema = mongoose.Schema({});
+const schema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter name"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter name"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter name"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please enter name"],
+  },
+  images: [
+    {
+      public_id: String,
+      url: String,
+    },
+  ],
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 export const Product = new mongoose.model("Product", schema);
